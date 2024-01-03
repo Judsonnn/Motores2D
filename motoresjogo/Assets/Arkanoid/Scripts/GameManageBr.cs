b7bd70af080a6879e4fab0f48c5e67a2d44a80c0
JUDSON LUAN DA SILVA DIAS <20221170150036@ifrn.local>
2024-01-03 15:01:29 -0300

    colocando jogo

---
+++ motoresjogo/Assets/Arkanoid/Scripts/GameManageBr.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagerB : MonoBehaviour
+{
+    public static GameManagerB instance;
+    public int vidas = 2;
+    public int tijolosRestantes;
+
+
+    public GameObject playerPrefab;
+    public GameObject ballPrefab;
+    public Transform playerSpawnPoint;
+    public Transform ballSpawnPoint;
+
+    public PlayerB playerAtual;
+    public BallB ballAtual;
+
+    public TextMeshProUGUI contador;
+    public TextMeshProUGUI msgFinal;
+
+    public bool segurando;
+    private Vector3 offset;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
